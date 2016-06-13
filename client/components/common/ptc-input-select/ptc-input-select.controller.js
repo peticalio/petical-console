0a1ebe01e64b5e328940e14219b9d7fa99da3b52
Ken Todoroki <ken.todoroki@majimena.org>
2016-06-13 14:12:00 +0900

    add input or select box

---
+++ client/components/common/ptc-input-select/ptc-input-select.controller.js
@@ -0,0 +1,37 @@
+(() => {
+  'use strict';
+
+  class PtcInputSelectController {
+    constructor() {
+      this.label = '項目名';
+      this.data = [];
+      this.minlength = 0;
+      this.maxlength = 250;
+      this.pattern = '';
+      this.message = '一覧から選択するか値を入力してください。';
+      this.required = false;
+    }
+
+    transform(value) {
+      console.log(value);
+      return value;
+    }
+  }
+
+  angular.module('petzApp.util')
+    .component('ptcInputSelect', {
+      templateUrl: 'components/common/ptc-input-select/ptc-input-select.html',
+      controller:  PtcInputSelectController,
+      bindings: {
+        form:        '=',
+        name:        '@',
+        label:       '@', // label
+        model:       '=', // ng-model
+        data:        '<', // data
+        minlength:   '@', // ng-minlength
+        maxlength:   '@', // ng-maxlength
+        message:     '@', // placeholder & error message
+        required:    '@'  // ng-required
+      }
+    });
+})();
